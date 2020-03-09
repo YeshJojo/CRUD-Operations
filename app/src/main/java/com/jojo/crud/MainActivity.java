1daa579a1f62449fbef86da56eccb04a60625ae6
Yeshwanth Jojode <36632375+YeshJojo@users.noreply.github.com>
2020-03-09 19:58:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/jojo/crud/MainActivity.java
+++ app/src/main/java/com/jojo/crud/MainActivity.java
@@ -3,8 +3,10 @@ package com.jojo.crud;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
@@ -16,48 +18,46 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton add;
-    Button update, remove;
+    Button update, remove, add;
+    dbHandler handler;
     LinearLayout layout;
-    studentDBHandler db;
-    TextView count;
-    Handler handler;
+    TextView record;
+    Handler repeat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        add = findViewById(R.id.button_add);
         update = findViewById(R.id.updateButt);
-        remove = findViewById(R.id.removeButt);
+        remove = findViewById(R.id.deleteButt);
+        add = findViewById(R.id.addButt);
         layout = findViewById(R.id.linearLayout);
-        count = findViewById(R.id.textViewCount);
-        db = new studentDBHandler(this);
-
-        handler = new Handler();
+        record = findViewById(R.id.records);
+        handler = new dbHandler(this);
+        repeat = new Handler();
         repeatTask();
-
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                final View formElementsView = inflater.inflate(R.layout.add_details, null, false);
-
+                final View form = inflater.inflate(R.layout.add_data, null, false);
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Add Data")
-                        .setView(formElementsView)
+                builder.setTitle("Add data")
+                        .setView(form)
                         .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                EditText name = formElementsView.findViewById(R.id.add_name);
-                                EditText sem = formElementsView.findViewById(R.id.add_sem);
-                                db.insertData(name.getText().toString(), sem.getText().toString());
-                                Toast.makeText(MainActivity.this, "Data Added Successfully",Toast.LENGTH_LONG).show();
-                                dialog.dismiss();
+                                EditText name = form.findViewById(R.id.add_name);
+                                EditText sem = form.findViewById(R.id.add_sem);
+                                if(name.getText().toString().isEmpty() && sem.getText().toString().isEmpty()){
+                                    Toast.makeText(getApplicationContext(), "Fill the empty fields", Toast.LENGTH_LONG).show();
+                                } else {
+                                    handler.insertData(name.getText().toString(), sem.getText().toString());
+                                    Toast.makeText(getApplicationContext(), "Data uploaded succcessfully", Toast.LENGTH_LONG).show();
+                                    dialog.dismiss();
+                                }
                             }
                         })
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -66,50 +66,54 @@ public class MainActivity extends AppCompatActivity {
                                 dialog.dismiss();
                             }
                         }).show();
-                readRecords();
-            }
-        });
-        remove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                final View view = inflater.inflate(R.layout.remove_details, null, false);
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Enter ID to remove Data")
-                        .setView(view)
-                        .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                EditText id = view.findViewById(R.id.remove_id);
-                                db.deleteData(id.getText().toString());
-                                Toast.makeText(MainActivity.this, "Data Removed Successfully",Toast.LENGTH_LONG).show();
-                                dialog.dismiss();
-                            }
-                        })
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        }).show();
-                readRecords();
             }
         });
         update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                final View view = inflater.inflate(R.layout.remove_details, null, false);
-
+                final View form = inflater.inflate(R.layout.remove_data, null, false);
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Enter ID to remove Data")
-                        .setView(view)
+                builder.setTitle("Enter ID to remove data")
+                        .setView(form)
                         .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                final EditText id = view.findViewById(R.id.remove_id);
-                                updateData(id.getText().toString());
+                                EditText id = form.findViewById(R.id.remove_id);
+                                if(id.getText().toString().isEmpty()){
+                                    Toast.makeText(getApplicationContext(), "Fill the empty fields", Toast.LENGTH_LONG).show();
+                                } else {
+                                    updateData(id.getText().toString());
+                                }
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).show();
+            }
+        });
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View form = inflater.inflate(R.layout.remove_data, null, false);
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Enter ID to remove data")
+                        .setView(form)
+                        .setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                EditText id = form.findViewById(R.id.remove_id);
+                                if(id.getText().toString().isEmpty()){
+                                    Toast.makeText(getApplicationContext(), "Fill the empty fields", Toast.LENGTH_LONG).show();
+                                } else {
+                                    handler.deleteData(id.getText().toString());
+                                    Toast.makeText(getApplicationContext(), "Data deleted succcessfully", Toast.LENGTH_LONG).show();
+                                    dialog.dismiss();
+                                }
                             }
                         })
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -121,13 +125,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     final Runnable status = new Runnable() {
         @Override
         public void run() {
             try {
-                readRecords();
+                readData();
             } finally {
-                handler.postDelayed(status, 500);
+                repeat.postDelayed(status, 500);
             }
         }
     };
@@ -135,10 +140,9 @@ public class MainActivity extends AppCompatActivity {
         status.run();
     }
 
-
     private void updateData(final String id) {
         LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        final View view1 = inflater.inflate(R.layout.add_details, null, false);
+        final View view1 = inflater.inflate(R.layout.add_data, null, false);
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("Enter Details to Update Data")
                 .setView(view1)
@@ -147,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         EditText name1 = view1.findViewById(R.id.add_name);
                         EditText sem1 = view1.findViewById(R.id.add_sem);
-                        db.updateData(id, name1.getText().toString(), sem1.getText().toString());
+                        handler.updateData(id, name1.getText().toString(), sem1.getText().toString());
                         Toast.makeText(MainActivity.this, "Data Updated Successfully",Toast.LENGTH_LONG).show();
                         dialog.dismiss();
                     }
@@ -158,34 +162,45 @@ public class MainActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 }).show();
+        readData();
     }
-    public void readRecords() {
+    @SuppressLint("SetTextI18n")
+    public void readData(){
         layout.removeAllViews();
-        List<studentDetails> students = new studentDBHandler(this).getAllData();
-        if (students.size() > 0) {
-            count.setVisibility(View.VISIBLE);
-            count.setGravity(Gravity.CENTER_HORIZONTAL);
-            count.setText(students.size() + " Records found!");
-            for (studentDetails obj : students) {
+        List<stdDetails> details = new dbHandler(this).getAllData();
+        if(details.size() > 0){
+            record.setVisibility(View.VISIBLE);
+            record.setGravity(Gravity.CENTER_HORIZONTAL);
+            String textViewData = "ID - Name - Sem";
+            TextView tv = new TextView(this);
+            tv.setPadding(10, 10, 10, 10);
+            tv.setText(textViewData);
+            tv.setTextSize(20);
+            tv.setGravity(Gravity.CENTER_HORIZONTAL);
+            layout.addView(tv);
+            record.setText(details.size() + " Records found!");
+            for (stdDetails obj : details) {
                 int id = obj.id;
                 String studentName = obj.name;
                 int studentEmail = obj.sem;
-                String textViewContents = studentName + " - " + studentEmail;
+                String textViewContents = id+ " - " + studentName + " - " + studentEmail;
                 TextView item = new TextView(this);
                 item.setPadding(10, 10, 10, 10);
                 item.setText(textViewContents);
                 item.setTextSize(20);
                 item.setGravity(Gravity.CENTER_HORIZONTAL);
-                item.setTag(Integer.toString(id));
                 layout.addView(item);
                 update.setVisibility(View.VISIBLE);
                 remove.setVisibility(View.VISIBLE);
             }
         }
         else {
-            count.setVisibility(View.VISIBLE);
-            count.setGravity(Gravity.CENTER_HORIZONTAL);
-            count.setText("No Records found!");
+            record.setVisibility(View.VISIBLE);
+            record.setGravity(Gravity.CENTER_HORIZONTAL);
+            record.setText("No Records found!");
+            update.setVisibility(View.GONE);
+            remove.setVisibility(View.GONE);
         }
     }
+
 }
